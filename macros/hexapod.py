61714f5c7cf82069cee44cf53ead48f666a161aa
Alberto López Sánchez <alopez@cells.es>
2021-05-25 13:59:12 +0200

    add set_pivot macro

--- macros/hexapod.py
+++ macros/hexapod.py
@@ -1,6 +1,7 @@
 from sardana.macroserver.macro import macro
 from sardana.macroserver.msparameter import Type
 
+
 @macro(interactive=True, param_def = [
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["command", Type.String, None, "Command to send to the hexapod"]
@@ -8,5 +9,20 @@ from sardana.macroserver.msparameter import Type
 def send_to_hexapode(self, hexapod, command):
 
     pool = self.getPools()[0]
+    controller_anser = pool.SendToController([hexapod, command])
+    self.output(f"{pool} controller answer: {controller_anser}")
+
+
+@macro(interactive=True, param_def = [
+    ["hexapod", Type.String, None, "Command receiver hexapod"],
+    ["x", Type.Float, None, "X coordinate of pivot"],
+    ["y", Type.Float, None, "y coordinate of pivot"],
+    ["z", Type.Float, None, "z coordinate of pivot"],
+    
+])
+def set_pivot(self, hexapod, x, y, z):
+
+    pool = self.getPools()[0]
+    command = f'self.hexapod.set_pivot({{ "X":{x}, "Y":{y}, "Z":{z} }})'
     controller_anser = pool.SendToController([hexapod, command])
     self.output(f"{pool} controller answer: {controller_anser}")
