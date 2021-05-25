6dbf2d96ee1512904197571756e711a883880ba3
Alberto López Sánchez <alopez@cells.es>
2021-05-25 11:22:47 +0200

    add macros to install script

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers\HexapodController.py
 del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers\hexapod.py
+del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\macroserver\macros\hexapod.py
 copy controllers\HexapodController.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers
 copy hexapod.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers
-cls
+copy macros\hexapod.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\macroserver\macros\hexapod.py
