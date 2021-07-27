24879408d966f4eb960a65880c23ac6834c7e33e
Alberto López Sánchez <alopez@cells.es>
2021-07-27 11:54:29 +0200

    clean repo

--- install.bat
+++
@@ -1,6 +0,0 @@
-del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers\HexapodController.py
-del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers\hexapod.py
-del C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\macroserver\macros\hexapod.py
-copy controllers\HexapodController.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers
-copy hexapod.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\pool\poolcontrollers
-copy macros\hexapod.py C:\Users\sunlo\miniconda3\envs\sardana\Lib\site-packages\sardana\macroserver\macros\hexapod.py
--- install.py
+++
@@ -1,4 +0,0 @@
-# Configure the Pool to use the path where the controllers and macros are located.
-
-MacroServer MacroPath
-Pool PoolPath
