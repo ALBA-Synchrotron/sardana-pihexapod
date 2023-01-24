fbf6306a162d47e8338f4ac59d68a7a9087e759b
Jordi Aguilar Larruy <jalarruy@axt.email>
2023-01-24 12:27:47 +0100

    pep8 improvements

--- sardana_pihexapod/macros/hexapod.py
+++ sardana_pihexapod/macros/hexapod.py
@@ -2,7 +2,7 @@ from sardana.macroserver.macro import macro
 from sardana.macroserver.msparameter import Type
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["command", Type.String, None, "Command to send to the hexapod"]
 ])
@@ -13,12 +13,11 @@ def send_to_hexapod(self, hexapod, command):
     self.output(f"{pool} controller answer: {controller_anser}")
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["x", Type.Float, None, "X coordinate of pivot"],
     ["y", Type.Float, None, "y coordinate of pivot"],
     ["z", Type.Float, None, "z coordinate of pivot"],
-    
 ])
 def hexapod_set_pivot(self, hexapod, x, y, z):
 
@@ -28,7 +27,7 @@ def hexapod_set_pivot(self, hexapod, x, y, z):
     self.output(f"{pool} controller answer: {controller_anser}")
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["name", Type.String, None, "Name of the new coordinate system"],
     ["x", Type.Float, None, "X coordinate of the new coordinate system"],
@@ -41,12 +40,13 @@ def hexapod_set_pivot(self, hexapod, x, y, z):
 def hexapod_new_coordinate_system(self, hexapod, name, x, y, z, u, v, w):
 
     pool = self.getPools()[0]
-    command = f'self.hexapod.new_coordinate_system("{name}", {{ "X":{x}, "Y":{y}, "Z":{z}, "U":{u}, "V":{v}, "W":{w} }})'
+    command = f'self.hexapod.new_coordinate_system("{name}",'\
+              f' {{ "X":{x}, "Y":{y}, "Z":{z}, "U":{u}, "V":{v}, "W":{w} }})'
     controller_anser = pool.SendToController([hexapod, command])
     self.output(f"{pool} controller answer: {controller_anser}")
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["name", Type.String, None, "Name of the new coordinate system"],
 ])
@@ -58,7 +58,7 @@ def hexapod_enable_coordinate_system(self, hexapod, name):
     self.output(f"{pool} controller answer: {controller_anser}")
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
 ])
 def hexapod_disable_coordinate_system(self, hexapod):
@@ -69,7 +69,7 @@ def hexapod_disable_coordinate_system(self, hexapod):
     self.output(f"{pool} controller answer: {controller_anser}")
 
 
-@macro(interactive=True, param_def = [
+@macro(interactive=True, param_def=[
     ["hexapod", Type.String, None, "Command receiver hexapod"],
     ["child", Type.String, None, "Name of the child1 coordinate system"],
     ["parent", Type.String, None, "Name of the parent coordinate system"],
@@ -77,7 +77,7 @@ def hexapod_disable_coordinate_system(self, hexapod):
 def hexapod_set_parent_coordinate_system(self, hexapod, child, parent):
 
     pool = self.getPools()[0]
-    command = f'self.hexapod.set_parent_coordinate_system(["{child}"], "{parent}")'
+    command =\
+        f'self.hexapod.set_parent_coordinate_system(["{child}"], "{parent}")'
     controller_anser = pool.SendToController([hexapod, command])
     self.output(f"{pool} controller answer: {controller_anser}")
-
